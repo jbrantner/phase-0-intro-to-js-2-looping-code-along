6aaf3d007da0d8c0c5a48808c3500e0650b8d0fe
Jordan Brantner <thejordanbrantner@gmail.com>
2023-07-10 20:36:57 -0400

    completed lab.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+
+
+
+function writeCards(names) {
+    let message = [];
+    for (let i = 0; i < names.length; i++) {
+    let array = `Thank you, ${names[i]}, for the wonderful surprise gift!`;
+    message.push(array);
+    }
+    return message; 
+}
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number--;
+        
+    }
+   
+
+};
